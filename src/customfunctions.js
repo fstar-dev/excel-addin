a8a94c6496e37aa513c0ae2ebcaf69302f22da8c
fstar-dev <defin.gonzales.92@gmail.com>
2018-10-26 07:52:03 -0300

    Create the addin

---
+++ src/customfunctions.js
@@ -0,0 +1,29 @@
+﻿function consultaLogradouro(cep) {
+  return fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => {
+      console.log(response)
+      resolve(response.logradrouro);
+    });
+};
+
+CustomFunctionMappings.CONSULTALOGRADOURO = consultaLogradouro;
+
+
+// function add(first, second){
+//   return first + second;
+// }
+
+// function increment(incrementBy, callback) {
+//   var result = 0;
+//   var timer = setInterval(function() {
+//     result += incrementBy;
+//     callback.setResult(result);
+//   }, 1000);
+
+//   callback.onCanceled = function() {
+//     clearInterval(timer);
+//   };
+// }
+
+// CustomFunctionMappings.ADD = add;
+// CustomFunctionMappings.INCREMENT = increment;
